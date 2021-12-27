5808c9fd438b524b8537d1514ed0e3ee7c3c20ba
mode9 <glee0628@gmail.com>
2021-12-28 01:30:21 +0900

    feat: 맵 컨트롤 이벤트

--- components/map/controls/location.tsx
+++ components/map/controls/location.tsx
@@ -1,4 +1,6 @@
 import {Container, ControlButton, Label, Svg} from "./_base";
+import Spinner from "../../spinner";
+import {NaverMap} from "../../../core/mapManager/naver";
 
 const Location = () => {
     return (
@@ -10,16 +12,17 @@ const Location = () => {
 }
 
 const LocationControl = () => (
-    <Container>
+    <Container id="geolocation-container">
         <ControlButton>
             <Location />
             <Label>내 위치 찾기</Label>
         </ControlButton>
+        <Spinner hidden={true} />
     </Container>
 )
 
 export class GeolocationHandler {
-    mapManager: any;
+    mapManager: NaverMap;
     defaultZoom: number;
     constructor(mapManager: any) {
         this.mapManager = mapManager;
@@ -31,18 +34,21 @@ export class GeolocationHandler {
         const x = position.coords.latitude;
         const y = position.coords.longitude;
         const mapManager = this.mapManager;
-        const pos = new mapManager.getLatLng(x, y);
+        const pos = mapManager.getLatLng(x, y);
         const zoom = this.defaultZoom;
-        function endCenterChanged () {
-            mapManager.setZoom(zoom);
+
+        const zoomEqual = mapManager.getZoom() === zoom;
+        const posEqual = pos.equals(mapManager.getCenter());
+
+        if (zoomEqual && posEqual) {
+            return Promise.all([]);
+        } else if (zoomEqual) {
+            return mapManager.panTo(pos);
+        } else if (posEqual) {
+            return mapManager.setZoom(zoom);
         }
-        // this.mapManager.module.maps.Manager
-        mapManager.addListenerOnce('idle', endCenterChanged)
-        mapManager.panTo(pos);
-        // setTimeout(() => {
-        //     kakao.maps.event.removeListener(map, 'idle', endCenterChanged);
-        // }, 1000);
-        // callback();
+
+        return mapManager.panTo(pos).then(() => mapManager.setZoom(zoom));
     }
 
     geoError (error: GeolocationPositionError) {
@@ -58,24 +64,29 @@ export class GeolocationHandler {
         if (error.code === 1) {
             alert('내 위치 확인을 허용해주세요.')
         }
-        // callback();
+    }
+
+    getPosition = (): Promise<GeolocationPosition> => {
+        const defaultOption = {
+            maximumAge: 1000 * 3600,
+            timeout: 1000 * 5,
+        }
+        return new Promise((resolve, reject) => {
+            if (!('geolocation' in navigator)) {
+                reject('지원하지 않는 브라우저입니다.');
+            }
+            navigator.geolocation.getCurrentPosition(resolve, reject, defaultOption);
+        });
     }
 
     moveTo = () => {
-        if ('geolocation' in navigator) {
-            navigator.geolocation.getCurrentPosition(this.geoSuccess, this.geoError);
-        } else {
-            alert('지원하지 않는 브라우저입니다.');
-        }
-    }
-}
-
-export const moveToCurrentPosition = (mapManager: any) => {
-    const locationHandler = new GeolocationHandler(mapManager);
-    return () => {
+        const geoSuccess = this.geoSuccess;
+        const geoError = this.geoError;
+        return this.getPosition()
+            .then((position) => geoSuccess(position))
+            .catch((error: GeolocationPositionError) => geoError(error))
 
     }
 }
-
 
 export default LocationControl;
--- components/map/index.tsx
+++ components/map/index.tsx
@@ -2,27 +2,17 @@ import {renderToString} from "react-dom/server";
 import React, {useEffect, useRef, useState} from "react";
 import Script from "next/script";
 import Pharmacy from "../../core/pharmacies";
-import styled from "styled-components";
-
-import {SpinnerTwoAlt} from "css.gg/icons/all";
 import InfoControl from "./controls/info";
-import LocationControl, {GeolocationHandler, moveToCurrentPosition} from "./controls/location";
-import Marker from "./marker";
-import initMarkerClustering from "./markerClustering/clustering";
-import MarkerCluster from "./markerClustering";
-import FindNearest from './controls/findNearest';
+import LocationControl, {GeolocationHandler} from "./controls/location";
+import FindNear from './controls/findNear';
+import Spinner from "../spinner";
+import {NaverMap} from "../../core/mapManager/naver";
+import {useTransition} from '@react-spring/web';
+import Modal from "../modal";
 
 const infoControlHtml = renderToString(<InfoControl />);
 const locationControlHtml = renderToString(<LocationControl />)
-const nearestControlHtml = renderToString(<FindNearest />);
-const clusterHtml = renderToString(<MarkerCluster />);
-
-type MarkerOption = {
-    latLng: any,
-    label: string|undefined,
-    disabled?: boolean,
-    onClick?: () => any,
-}
+const nearControlHtml = renderToString(<FindNear />);
 
 interface MapOptions {
     data: Pharmacy[];
@@ -32,174 +22,22 @@ interface MapOptions {
     isHoliday?: boolean;
 }
 
-interface mapInterface {
-    module: any;
-    mapDiv: HTMLDivElement|null;
-    map: any;
-    markers: any[];
-    markerCluster: any;
-
-    getInitOptions: () => {[key: string]: any};
-    init: (module: any, mapDiv: HTMLDivElement) => void;
-
-    getMarkerHtml: () => string;
-    getMarkerClusterHtml: () => string;
-
-    insertMarker: (props: MarkerOption) => void;
-    insertCustomControl: (controlHtml: string, options: {position: string}, onClick: () => void) => any;
-    clearMarkers: () => void;
-    removeMarker: (marker: any) => void;
-    setMarkerClustering: () => void;
-    updateMarkerClustering: (markers: any[]) => void;
-}
-
-class NaverMap implements mapInterface {
-    module: any;
-    mapDiv: HTMLDivElement | null;
-    map: any;
-    markers: any[];
-    markerCluster: any;
-
-    constructor() {
-        this.module = null;
-        this.mapDiv = null;
-        this.markers = [];
-        // this.getLatLng = this.getLatLng.bind(this);
-        // this.setZoom = this.setZoom.bind(this);
-        // this.addListenerOnce = this.addListenerOnce.bind(this);
-        // this.panTo = this.panTo.bind(this);
-    }
-
-    getInitOptions = () => {
-        const coords = [126.96072340180352, 37.54425411510226];
-        return {
-            center: this.module.maps.LatLng(coords[1], coords[0]),
-            zoom: 14,
-            useStyleMap: true,
-            zoomControl: true,
-            zoomControlOptions: {
-                style: this.module.maps.ZoomControlStyle.SMALL,
-                position: this.module.maps.Position.LEFT_CENTER
-            },
-            tileSpare: 1,
-        }
-    }
-
-    getMarkerHtml = (disabled?: boolean, label?: string): string => {
-        return renderToString(<Marker disabled={disabled} label={label} />);
-    }
-
-    getMarkerClusterHtml = () => {
-        return clusterHtml;
-    }
-
-    getMarkerClusterIcon = () => {
-        return {
-            content: this.getMarkerClusterHtml(),
-            size: this.module.maps.Size(40, 40),
-            anchor: this.module.maps.Point(10, 20)
-        }
-    }
-
-    init = (module: any, mapDiv: HTMLDivElement) => {
-        this.module = module;
-        this.mapDiv = mapDiv;
-        // @ts-ignore
-        this.map = new this.module.maps.Map(this.mapDiv, this.getInitOptions());
-    }
-
-    insertCustomControl = (controlHtml: string, options: {position: string}, onClick: (_controller?: any) => void): any => {
-        if (!this.module || !this.map) {
-            throw new Error('mapManager not initialized yet.');
-        }
-        const control = new this.module.maps.CustomControl(controlHtml, options);
-        control.setMap(this.map);
-        onClick && this.module.maps.Event.addDOMListener(control.getElement(), 'click', () => onClick(control));
-        return control;
-    }
-
-    insertMarker = (props: MarkerOption) => {
-        // @ts-ignore
-        const marker = new this.module.maps.Marker({
-            map: this.map,
-            position: props.latLng,
-            zIndex: 100,
-            icon: {
-                content: this.getMarkerHtml(props.disabled, props.label),
-                anchor: new this.module.maps.Point(10, 20)
-            },
-            title: props.label
-        })
-        marker.disabled = props.disabled;
-        if (props.onClick) {
-            this.module.maps.Event.addListener(marker, 'click', props.onClick)
-        }
-        this.markers.push(marker);
-    }
-
-    clearMarkers = () => {
-        for (let i=0; i < this.markers.length; i++) {
-            this.markers[i]?.setMap(null);
-        }
-        this.markers = [];
-    }
-
-    removeMarker = (marker: any) => {
-        marker.setMap(null);
-        this.markers = this.markers.filter((row) => (!row.getPosition().equals(marker.getPosition())));
-    }
-
-    setMarkerClustering = () => {
-        this.markerCluster = initMarkerClustering(this.module, {
-            minClusterSize: 2,
-            maxZoom: 15,
-            map: this.map,
-            markers: this.markers,
-            disableClickZoom: false,
-            gridSize: 120,
-            icons: [this.getMarkerClusterIcon()],
-            indexGenerator: [10, 100, 200, 500, 1000],
-            stylingFunction: function(clusterMarker: any, count: number|string, members: any[]) {
-                const allDisabled = members.every((member: any) => member.disabled);
-                let markerNode: HTMLElement = clusterMarker.getElement().firstElementChild.firstElementChild;
-
-                if (allDisabled) {
-                    markerNode.style.background = '#8D8D8D';
-                }
-                markerNode.innerText = count.toString();
-            }
-        })
-    }
-
-    updateMarkerClustering = (markers: any[]) => {
-        this.markerCluster.setMarkers(markers);
-    }
-
-    getLatLng = (x: number, y: number): any => {
-        return new this.module.maps.LatLng(x, y)
-    }
-
-    setZoom = (zoom: number): void => {
-        this.map.setZoom(zoom, true);
-    }
-
-    addListenerOnce = (evt: string, callback: () => void) => {
-        this.module.maps.Event.once(this.map, evt, callback);
-    }
-
-    panTo = (latlng: any) => {
-        this.map.panTo(latlng);
-    }
-}
-
 const MapComponent = React.forwardRef((props: MapOptions, ref): React.ReactElement=> {
     const mapRef = useRef<HTMLDivElement>(null);
     const [pharms, setPharms] = useState<Pharmacy[]>([]);
-    const [loading, setLoading] = useState(true);
+    const [mapLoading, setMapLoading] = useState(true);
     const [initialized, setInitialized] = useState(false);
     const [moduleLoaded, setModuleLoaded] = useState(false);
-    const [mapManager, setMapManager] = useState<NaverMap>(new NaverMap());
-    const [controller, setController] = useState();
+    const [mapManager] = useState<NaverMap>(new NaverMap());
+    // const [findNearController, setFindNearController] = useState(null);
+    // const [locController, setLocController] = useState(null);
+    // const [infoController, setInfoController] = useState(null);
+    const [modalVisible, setModalVisible] = useState(false);
+    const transitions = useTransition(modalVisible, {
+        from: { opacity: 0, transform: "translateY(-40px)" },
+        enter: { opacity: 1, transform: "translateY(0px)" },
+        leave: { opacity: 0, transform: "translateY(-40px)" }
+    });
 
     React.useImperativeHandle(ref, () => ({
         hasModuleLoaded: (): boolean => moduleLoaded,
@@ -215,15 +53,6 @@ const MapComponent = React.forwardRef((props: MapOptions, ref): React.ReactEleme
     function handleModuleOnError (): void {
         alert('module loaded failed');
     }
-
-    function handleClickMarker (seq: number): () => void {
-        return () => {
-            console.log(seq)
-            console.log('marker clicked');
-            console.log(mapManager.markers[seq]);
-        }
-    }
-
     function hasModuleLoaded (): boolean {
         return moduleLoaded && window?.naver;
     }
@@ -242,48 +71,67 @@ const MapComponent = React.forwardRef((props: MapOptions, ref): React.ReactEleme
                     latLng: latlng,
                     label: pharmacy.name,
                     disabled: !pharmacy.isOpen(props.isHoliday),
-                    onClick: handleClickMarker(mapManager.markers.length),
+                    onClick: handleClickMarker,
                 })
             }
         }
         setPharms(pharmaciesInBounds);
     }
 
-    function handleClickNearest (_controller: any): void{
-        _controller.getElement().firstElementChild.style.display = 'none';
+    function handleClickMarker (this: any): void {
+            // console.log(seq)
+        console.log('marker clicked');
+        console.log(this);
+    }
+
+    function handleClickNear (this: any): void{
+        const findNearContainerNode = this.getElement().firstElementChild;
+        findNearContainerNode?.classList.remove('active');
         updateMarkers();
         mapManager.updateMarkerClustering(mapManager.markers);
         mapManager.markerCluster._redraw();
     }
 
-    function handleInitMap (): void {
+    function handleClickGeolocation (this: any): void {
         const geolocationHandler = new GeolocationHandler(mapManager);
-        mapManager.insertCustomControl(infoControlHtml, {position: mapManager.module.maps.Position.TOP_LEFT}, () => {});
-        mapManager.insertCustomControl(locationControlHtml, {position: mapManager.module.maps.Position.TOP_LEFT}, geolocationHandler.moveTo);
-        const control = mapManager.insertCustomControl(nearestControlHtml, {position: mapManager.module.maps.Position.BOTTOM_CENTER}, handleClickNearest);
-        // setController(control);
-        // mapManager.module.maps.Event.addListener(mapManager.map, "zoom_changed", (zoom: number) => {
-        //
-        // })
-        updateMarkers();
+        const containerNode = this.getElement().firstElementChild;
+        containerNode?.classList.add('loading');
+        geolocationHandler.moveTo()
+            .then(() => {
+                console.log('geolocation ended');
+                containerNode?.classList.remove('loading');
+                const _findNearController = mapManager.controls.find(control => control.name === 'findNear');
+                _findNearController?.getElement().firstElementChild.click();
 
-        window.naver.maps.Event.addListener(mapManager.map, 'bounds_changed', handleBoundsChanged(control));
-        window.naver.maps.Event.addListener(mapManager.map, 'idle', handleIdle);
-        mapManager.setMarkerClustering();
-        setLoading(false);
+            });
     }
 
-    function handleBoundsChanged (_controller: any): () => void {
-        return () => {
-            console.log('bounds changed');
-            const displayFindNearestThrottle = 14;
-            const zoom = mapManager.map.getZoom();
-            if (zoom < displayFindNearestThrottle && controller) {
-                _controller.getElement().firstElementChild.style.display = 'none';
-            } else {
-                _controller.getElement().firstElementChild.style.display = 'block';
-            }
-        }
+    function handleClickInfo (this: any) {
+        console.log(this);
+
+        setModalVisible(true);
+    }
+
+    function handleInitMap (): void {
+        const _findNearController = mapManager.insertCustomControl(nearControlHtml, {position: mapManager.module.maps.Position.BOTTOM_CENTER}, handleClickNear);
+        const _infoController = mapManager.insertCustomControl(infoControlHtml, {position: mapManager.module.maps.Position.TOP_LEFT}, handleClickInfo);
+        const _locController = mapManager.insertCustomControl(locationControlHtml, {position: mapManager.module.maps.Position.TOP_LEFT}, handleClickGeolocation);
+        _findNearController.name = 'findNear';
+        _infoController.name = 'info';
+        _locController.name = 'geolocation';
+        updateMarkers();
+
+        window.naver.maps.Event.addListener(mapManager.map, 'bounds_changed', handleBoundsChanged.bind(_findNearController));
+        window.naver.maps.Event.addListener(mapManager.map, 'idle', handleIdle);
+        mapManager.setMarkerClustering();
+        setMapLoading(false);
+    }
+
+    function handleBoundsChanged (this: any) {
+        const displayFindNearThrottle = 14;
+        const zoom = mapManager.map.getZoom();
+        const findNearContainerNode = this.getElement().firstElementChild;
+        findNearContainerNode.classList.toggle('active', zoom >= displayFindNearThrottle);
     }
 
     function handleIdle (): void {
@@ -300,24 +148,23 @@ const MapComponent = React.forwardRef((props: MapOptions, ref): React.ReactEleme
         mapManager.updateMarkerClustering(visibleMarkers);
     }
 
+    // useEffect(() => {
+    //     if (!findNearController) return;
+    //
+    //
+    //     setInfoController(_infoController);
+    //     setLocController(_locController)
+    // }, [findNearController])
+
     useEffect(() => {
         if (!hasModuleLoaded() || !mapRef.current || initialized) return;
         const { naver } = window as any;
-
         mapManager.init(naver, mapRef.current);
-
         naver.maps.Event.once(mapManager.map, 'init_stylemap', handleInitMap)
         setInitialized(true);
-    }, [moduleLoaded, pharms, props.isHoliday, props.filterInBounds]);
+    }, [moduleLoaded, initialized]);
 
-    const SpinnerWrapper = styled.div`
-      width: 100%;
-      height: 100%;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      background-color: #F6F6F6;
-    `;
+
 
     return (
         <>
@@ -327,12 +174,18 @@ const MapComponent = React.forwardRef((props: MapOptions, ref): React.ReactEleme
                 onLoad={handleModuleOnLoad}
                 onError={handleModuleOnError}
             />
-            {loading && (
-                <SpinnerWrapper>
-                    <SpinnerTwoAlt />
-                </SpinnerWrapper>
-            )}
-            <div id="map" ref={mapRef} />
+            {mapLoading && <Spinner />}
+            <div id="map" ref={mapRef} >
+                {transitions( ( styles, item ) =>
+                    item && (
+                        <Modal
+                            style={styles}
+                            closeModal={() => setModalVisible(false)}
+                        />
+                    )
+                )}
+            </div>
+
 
         </>
     )
